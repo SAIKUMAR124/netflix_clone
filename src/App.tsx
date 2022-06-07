c0a035aa30839c63c2937fb3d9ea0422d009aeb5
dev global <pavankumar.s12459@gmail.com>
2022-06-07 08:12:47 +0530

    added route to initial page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import Initial_page from "./components/initial_page/index";
 
 function App() {
   return (
     <main>
-      <Initial_page />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Initial_page />} />
+        </Routes>
+      </Router>
     </main>
   );
 }
